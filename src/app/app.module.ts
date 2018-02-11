960ff64d080c4c55c1d70736c43d25a5f31b8aca
Diego Gaitan <felipe.gaitan.81@gmail.com>
2018-02-10 21:45:18 -0500

    Session 2, Class 17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule} from '@angular/http';
 import { AppComponent } from './app.component';
+import { HiWorldComponent } from './hi-world/hi-world.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HiWorldComponent
   ],
   imports: [
     BrowserModule,
