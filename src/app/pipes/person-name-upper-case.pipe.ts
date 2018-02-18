c90e5a2ef502190e73bb0eef9f76865ec108cb13
Diego Gaitan <felipe.gaitan.81@gmail.com>
2018-02-18 18:51:03 -0500

    Cleaning code for example 23
    End of section 2 class 24
    
    Pipes

---
+++ src/app/pipes/person-name-upper-case.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'personNameUpperCase'
+})
+export class PersonNameUpperCasePipe implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+
+    return 'Diego';
+  }
+
+}
